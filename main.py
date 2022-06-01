cf2773a27313e39c4288d81bec5d3b334bffce63
unknown <xlmriosx@gmail.com>
2022-06-01 17:00:02 -0300

    C0001 - Added files

---
+++ main.py
@@ -0,0 +1,7 @@
+def additon_array(array):
+    result = 0
+    for i in array:
+        result += i
+    return 0
+
+#Link: https://ellibrodepython.com/python-testing
---
+++ test.py
@@ -0,0 +1,4 @@
+from main import additon_array
+
+def test_additon_array():
+    assert additon_array([1,2,3]) == 6
