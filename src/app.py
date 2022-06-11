fc8925133ee460c5878cc71ac1c5db32e09d3dcd
unknown <xlmriosx@gmail.com>
2022-06-11 16:14:30 -0300

    New build

--- src/app.py
+++ src/app.py
@@ -1,15 +1,24 @@
 '''app.py'''
 
-from flask import Flask
+from flask import Flask, render_template, request
+
+def suma(a=0,b=0):
+    return (a + b)
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
     '''
     This is a docstring
     '''
-    return "<h1>hello continuos world</h1>"
+    if request.method == 'POST':
+        num1 = request.form['num1']
+        num2 = request.form['num2']
+        sum12 = suma(int(num1), int(num2))
+        return render_template('suma.html', suma=sum12)
+
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
---
+++ src/test.py
@@ -0,0 +1,9 @@
+'''test.py'''
+
+from app import suma
+
+def test_suma():
+    '''
+    This is a docstring
+    '''
+    assert suma(2,3)==5
